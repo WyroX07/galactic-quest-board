7a4a16aba6ac5d7d4bf228e148d455b37ed9b626
Maxence <la226646@student.helha.be>
2026-08-14 15:24:46 +0200

    Nettoyage supplementaire de HelloApplication

--- src/main/java/com/example/demomapprojet/Launcher.java
+++ src/main/java/com/example/demomapprojet/Launcher.java
@@ -1,14 +1,9 @@
 package com.example.demomapprojet;
 
-import com.google.gson.Gson;
-
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-
         Application.launch(HelloApplication.class, args);
-        System.out.println(new Gson().toJson("ok"));
-
     }
 }
